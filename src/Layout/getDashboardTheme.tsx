20b1a28ec49ebedbe0b7f346d4c5099cc9ff272d
iamzubair6 <zubair.rahman89@gmail.com>
2024-09-09 22:28:47 +0600

    theme -> inputs theme added

--- src/Layout/getDashboardTheme.tsx
+++ src/Layout/getDashboardTheme.tsx
@@ -1,9 +1,9 @@
 import type {} from "@mui/material/themeCssVarsAugmentation";
 import { ThemeOptions, PaletteMode } from "@mui/material/styles";
-import type {} from "@mui/x-date-pickers/themeAugmentation";
+// import type {} from "@mui/x-date-pickers/themeAugmentation";
 import type {} from "@mui/x-charts/themeAugmentation";
 import type {} from "@mui/x-data-grid/themeAugmentation";
-import type {} from "@mui/x-tree-view/themeAugmentation";
+// import type {} from "@mui/x-tree-view/themeAugmentation";
 import { getDesignTokens } from "./themePrimitives";
 import {
   chartsCustomizations,
